0fcfbd86d888a494e5eec47efae2e3163cb66c7f
Leanne de Goeij <lr.degoey@gmail.com>
2021-01-27 11:45:50 +0100

    Meer persoonlijke info

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
 $( document ).ready(function() {
-    // if($('.tutorials').length > 0 && $('.tutorialTag').length > 0){
-    if($('.tutorials').length > 0){
-        console.log('yes');
-        // $.each($('.tutorialTag'), function( index, value ) {
-        //     $(this).click(function() {
-        //         let tagName = $(this).attr('data-name');
-        //         console.log(tagName);
-        //     });
-        // });
-    }
+
 });
